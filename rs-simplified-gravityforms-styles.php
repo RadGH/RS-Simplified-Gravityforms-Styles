bcca142a7011d2d52e25b76ecf760502aac4a30c
Radley <radleygh@gmail.com>
2018-03-31 17:01:15 -0700

    Added default tabindex of 20, plus 20 for each form thereafter

--- rs-simplified-gravityforms-styles.php
+++ rs-simplified-gravityforms-styles.php
@@ -11,17 +11,35 @@ Author URI: https://radleysustaire.com/
 define( 'RS_SGFS_URL', untrailingslashit(plugin_dir_url( __FILE__ )) );
 define( 'RS_SGFS_PATH', dirname(__FILE__) );
 
+// Add our stylesheet
 function rs_sgfs_enqueue_scripts() {
 	wp_enqueue_style( 'rs_sgfs', RS_SGFS_URL . '/styles/rs-gravityforms.css' );
 }
 add_action( 'wp_enqueue_scripts', 'rs_sgfs_enqueue_scripts', 8 );
 
+// Add a default tabindex value if none is given in the shortcode to prevent tabbing to the wrong element
+// Start at 20, and add 20 for each form thereafter.
+function rs_sgfs_add_tabindex( $out, $pairs, $atts, $shortcode ) {
+	// Only apply when a value isn't given by the shortcode.
+	if ( !isset($atts['tabindex']) ) {
+		static $increment = 0;
+		$increment += 20;
+		
+		$out['tabindex'] = $increment;
+	}
+	
+	return $out;
+}
+add_filter( 'shortcode_atts_gravityforms', 'rs_sgfs_add_tabindex', 10, 4 );
+
+// Disable Gravity Forms CSS in the options menu when the plugin is activated
 function rs_sgfs_activate_plugin() {
 	// Toggle gravity form CSS setting
 	update_option( 'rg_gforms_disable_css', 1 );
 }
 register_activation_hook( __FILE__, 'rs_sgfs_activate_plugin' );
 
+// Re-enable Gravity Forms CSS in the options menu when the plugin is activated
 function rs_sgfs_deactivate_plugin() {
 	// Toggle gravity form CSS setting
 	update_option( 'rg_gforms_disable_css', 0 );
